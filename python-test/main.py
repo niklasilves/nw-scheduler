83d7c760d6cd374bc99ecba268f028c947e21581
Johan Jakberger <johanjakberger@gmail.com>
2025-09-03 22:06:06 +0200

    small fixes for python script to get correct dates

--- python-test/main.py
+++ python-test/main.py
@@ -3,27 +3,47 @@ import argparse
 from datetime import datetime
 from datetime import timedelta
 
-FROMTHURSTOSUN = timedelta(3)
-FROMSUNTOTHURS = timedelta(4)
-rows = []
+FROMTHURSTOSUN = 3
+FROMSUNTOTHURS = 4
+TIMEDELTAWEEK = timedelta(7)
+rows: list[tuple[str, str, str, str]] = []
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-d','--date',type=str)
 args = parser.parse_args()
 
 currentDate = datetime.strptime(args.date, "%Y-%m-%d").date()
-currentDate +=FROMTHURSTOSUN
-print(currentDate)
-currentDate +=FROMSUNTOTHURS
-print(currentDate)
+# currentDate +=FROMTHURSTOSUN
+# print(currentDate)
+# currentDate +=FROMSUNTOTHURS
+# print(currentDate)
 
-with open("testfile.csv", mode='r', encoding="utf-8-sig") as file:
+with open("../input/mikrofoner.csv", mode='r', encoding="utf-8-sig") as file:
     csvFile = csv.DictReader(file)
 
-    parseTest = datetime    
     for lines in csvFile:
-        print(lines)
-        date = datetime.strptime(lines["Date"], "%Y-%m-%d").date()
-        lines["Date"] = str(date + FROMTHURSTOSUN)
-        print(lines)
+        # print(lines)
+        date_multiplier = int(lines["Date"])
+        # print(currentDate + timedelta(date_multiplier*7))
+        rows.append(
+            (
+                str(currentDate + timedelta(date_multiplier*7)),
+                lines["Person"],
+                lines["Type"],
+                lines["TypeName"],
+            )
+        )
+        rows.append(
+            (
+                str(currentDate + timedelta((date_multiplier*7)+FROMTHURSTOSUN)),
+                lines["Person"],
+                lines["Type"],
+                lines["TypeName"],
+            )
+        )
+        # print(currentDate + timedelta((date_multiplier*7)+FROMTHURSTOSUN))
+rows.sort()
+for row in rows:
+    print(row)
+        # lines["Date"] = str(date + FROMTHURSTOSUN)
     
