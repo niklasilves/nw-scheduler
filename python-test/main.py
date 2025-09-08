fa7b0a40b12032a3f39dba5e7ad1bf466cf345fd
Johan <johan@alfasafe.se>
2025-09-08 08:54:34 +0200

    Finishing up details on python script for CSV files

--- python-test/main.py
+++ python-test/main.py
@@ -2,48 +2,57 @@ import csv
 import argparse
 from datetime import datetime
 from datetime import timedelta
-
-FROMTHURSTOSUN = 3
-FROMSUNTOTHURS = 4
-TIMEDELTAWEEK = timedelta(7)
-rows: list[tuple[str, str, str, str]] = []
+VERSION = '0.1.0'
+VERSION_PRINT = 'NW-Scripter - version {}'.format(VERSION)
+DAYS = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-d','--date',type=str)
+parser.add_argument('-v','--version', action='store_true', help='Prints the current version of program')
+parser.add_argument('-d','--date',type=str, help='The date to start adding from')
+parser.add_argument('-sat','--saturday', action='store_true', help='Sets weekend meeting to saturday, default is sunday')
 args = parser.parse_args()
 
-currentDate = datetime.strptime(args.date, "%Y-%m-%d").date()
-# currentDate +=FROMTHURSTOSUN
-# print(currentDate)
-# currentDate +=FROMSUNTOTHURS
-# print(currentDate)
 
-with open("../input/mikrofoner.csv", mode='r', encoding="utf-8-sig") as file:
-    csvFile = csv.DictReader(file)
+def run_program():
+    rows: list[tuple[str, str, str, str]] = []
 
-    for lines in csvFile:
-        # print(lines)
-        date_multiplier = int(lines["Date"])
-        # print(currentDate + timedelta(date_multiplier*7))
-        rows.append(
-            (
-                str(currentDate + timedelta(date_multiplier*7)),
-                lines["Person"],
-                lines["Type"],
-                lines["TypeName"],
+    mid_week_meeting_day:int = datetime.strptime(args.date, "%Y-%m-%d").date().weekday()
+    weekend_meeting_day:int = 5 if args.saturday else 6
+    meetings_diff = weekend_meeting_day - mid_week_meeting_day
+    print("Starting script for automated setup of dates with meeting-days:\n\n- {} \n- {}\n".format(DAYS[mid_week_meeting_day], DAYS[weekend_meeting_day]))
+
+    with open("./input/base_schedule.csv", mode='r', encoding="utf-8-sig") as file:
+        csvFile = csv.DictReader(file)
+        current_date = datetime.strptime(args.date, "%Y-%m-%d").date()
+
+        for lines in csvFile:
+            date_multiplier = int(lines["Date"])
+            rows.append(
+                (
+                    str(current_date + timedelta(date_multiplier*7)),
+                    lines["Person"],
+                    lines["Type"],
+                    lines["TypeName"],
+                )
             )
-        )
-        rows.append(
-            (
-                str(currentDate + timedelta((date_multiplier*7)+FROMTHURSTOSUN)),
-                lines["Person"],
-                lines["Type"],
-                lines["TypeName"],
+            rows.append(
+                (
+                    str(current_date + timedelta((date_multiplier*7)+meetings_diff)),
+                    lines["Person"],
+                    lines["Type"],
+                    lines["TypeName"],
+                )
             )
-        )
-        # print(currentDate + timedelta((date_multiplier*7)+FROMTHURSTOSUN))
-rows.sort()
-for row in rows:
-    print(row)
-        # lines["Date"] = str(date + FROMTHURSTOSUN)
-    
+    rows.sort()
+    with open("./output/"+str(current_date)+"-output.csv", "w", encoding="UTF-8") as f:
+        f.write("Date, Person, Type, TypeName\n")
+        for row in rows:
+            f.write("%s,%s,%s,%s\n" % (row[0], row[1], row[2], row[3]))
+
+        f.close()
+
+if __name__ == '__main__':
+    if args.version:
+        print(VERSION_PRINT)
+    else:
+        run_program()
